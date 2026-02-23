9143d4d54af951000d3af1383d220861ef69e9d5
Antonio Cumbe <cumbeantonio292@gmail.com>
2026-02-23 09:27:49 -0800

    Payment, one-to-one assopciation

---
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -0,0 +1,53 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long id;
+
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Payment() {
+    }
+
+    // GETTERS
+    public Long getId() { return id; }
+    public Instant getMoment() { return moment; }
+    public Order getOrder() { return order; }
+
+    // SETTERS
+    public void setId(Long id) { this.id = id; }
+    public void setMoment(Instant moment) { this.moment = moment; }
+    public void setOrder(Order order) { this.order = order; }
+
+    @Override
+    public int hashCode() { return Objects.hash(id); }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Payment)) return false;
+        Payment other = (Payment) obj;
+        return Objects.equals(id, other.id);
+    }
+}
