9246549480868c8f45e82518f7f3f22686e70601
Antonio Cumbe <cumbeantonio292@gmail.com>
2026-02-25 01:41:10 -0800

    Exception handling delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,35 +15,37 @@ import jakarta.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class) // ✅ import correto
-    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-        String error = "Resource not found";
-        HttpStatus status = HttpStatus.NOT_FOUND;
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e,
+			HttpServletRequest request) {
 
-        StandardError err = new StandardError(
-            Instant.now(),
-            status.value(),
-            error,
-            e.getMessage(),
-            request.getRequestURI()
-        );
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
 
-        return ResponseEntity.status(status).body(err);
-    }
+		StandardError err = new StandardError(
+				Instant.now(),
+				status.value(),
+				error,
+				e.getMessage(),
+				request.getRequestURI());
 
-    @ExceptionHandler(Exception.class) // Para qualquer outro erro
-    public ResponseEntity<StandardError> genericException(Exception e, HttpServletRequest request) {
-        String error = "Internal server error";
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		return ResponseEntity.status(status).body(err);
+	}
 
-        StandardError err = new StandardError(
-            Instant.now(),
-            status.value(),
-            error,
-            e.getMessage(),
-            request.getRequestURI()
-        );
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e,
+			HttpServletRequest request) {
 
-        return ResponseEntity.status(status).body(err);
-    }
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+
+		StandardError err = new StandardError(
+				Instant.now(),
+				status.value(),
+				error,
+				e.getMessage(),
+				request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+	}
 }
